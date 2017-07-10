4bda959e4fcfd47d41917e1f17a310f5063a8b12
Amy kang <amykang@amys-macbook-pro.local>
2017-07-09 23:53:45 -0400

    Initial Commit

---
+++ menu bars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  menu bars
+//
+//  Created by Amy kang on 2017-07-09.
+//  Copyright © 2017 Amy kang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
